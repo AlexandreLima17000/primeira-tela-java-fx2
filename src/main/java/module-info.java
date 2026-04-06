9228389d88372c3fc77cf1ef6d3e02b1f6512ebd
Adriel <instrutor.adriel@gmail.com>
2026-04-06 10:39:11 -0300

    Cria a estrutura inicial do projeto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.example.app_com_interface {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.app_com_interface to javafx.fxml;
+    exports org.example.app_com_interface;
+}
